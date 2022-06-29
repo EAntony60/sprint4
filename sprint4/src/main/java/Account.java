92454c225c1f4e60e16507a15faf1e7d52614f02
Eroshchuk Anton <eroschuk@gmail.com>
2022-06-29 18:05:01 +0300

    added tests

--- sprint4/src/main/java/Account.java
+++ sprint4/src/main/java/Account.java
@@ -11,10 +11,6 @@ public class Account {
         boolean isRightAndLeftSpaces = false;
         boolean result = false;
 
-        /*
-             Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
-             Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
-         */
         if ((name.length() >= 3) && (name.length() <= 19)) {isCorrespondingLength = true;}
 
         if ((name.indexOf(" ") != 0) && (name.lastIndexOf(" ") != name.length()-1)) {} else { isRightAndLeftSpaces = true;}
@@ -26,18 +22,6 @@ public class Account {
         }
 
         return result;
-//
-//
-//        if (((name.length() >= 3)
-//                && (name.length() <= 19))
-//                && (((name.indexOf(" ") == name.lastIndexOf(" "))
-//                && (name.indexOf(" ") != 0)
-//                && (name.lastIndexOf(" ") != name.length())))) {
-//            isCorrespondingLength = true;
-//        } else {
-//            isCorrespondingLength = false;
-//        }
-//        return  isCorrespondingLength;
     }
 
 }
